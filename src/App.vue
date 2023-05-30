c89a80ada35ae567dd5d382904dc6ac222a21709
adelinashahaj <adelinarucaj@icloud.com>
2023-05-30 15:31:28 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  import AppHeader from './components/AppHeader.vue';
+
+
+  export default {
+
+    components: {
+      AppHeader
+      
+    }
+  }
+</script>
+
+<template>
+   <AppHeader></AppHeader>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+
+</style>
